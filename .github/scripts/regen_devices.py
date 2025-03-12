c89cbd256a83b45eb2e6c16d2f2cec6a5bbceb7d
Karan Parashar <karan@pixelos.net>
2025-03-12 13:22:58 +0530

    Add workflow for auto updating devices json
    Change-Id: Iee4828a5d53094afde9b013cdc079498bbdd0ecb

---
+++ .github/scripts/regen_devices.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+
+device_list = []
+
+for device in os.listdir("API/devices"):
+    with open(f"API/devices/{device}", "r") as f:
+        device_data = json.load(f)
+
+    device_entry = {
+        "codename": device_data["codename"],
+        "codename_alt": device_data["codename_alt"],
+        "vendor": device_data["vendor"],
+        "model": device_data["model"],
+        "maintainer_name": " && ".join(
+            [maintainer["display_name"] for maintainer in device_data["maintainer"]]
+        ),
+        "frame": None,
+        "active": device_data["active"],
+    }
+
+    device_list.append(device_entry)
+
+
+devices_json = {"devices": device_list}
+
+with open("API/devices.json", "w") as f:
+    json.dump(devices_json, f, indent=4)
